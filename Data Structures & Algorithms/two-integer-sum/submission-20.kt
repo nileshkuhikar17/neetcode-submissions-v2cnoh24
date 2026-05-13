50b3ae1c499201e2f86e0fd0afe14342358202fd
Nilesh Kuhikar <65588647+nileshkuhikar17@users.noreply.github.com>
2026-05-13 21:16:58 +0530

    Add: two-integer-sum - submission-20

---
+++ Data Structures & Algorithms/two-integer-sum/submission-20.kt
@@ -0,0 +1,15 @@
+class Solution {
+    fun twoSum(nums: IntArray, target: Int): IntArray {
+        var prevMap=HashMap<Int,Int>()
+        for((i,n) in nums.withIndex()){
+            var diff=target-n
+            if(prevMap.contains(diff)){
+                return intArrayOf(prevMap[diff]!!,i)
+            }
+            prevMap[n]=i
+        
+        }
+        return intArrayOf()
+    }
+    
+}
